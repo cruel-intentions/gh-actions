b8d38bce4ba4277d551537235fa8c3a9838fa862
hugosenari <hugosenari@gmail.com>
2021-11-18 15:02:22 -0300

    fix(gh-actions): using ubuntu image

--- gh-actions.nix
+++ gh-actions.nix
@@ -5,9 +5,16 @@ in
   imports = [ ./gh-actions-options.nix ];
   config.files.yaml."/.github/workflows/ci-cd.yaml" = lib.mkIf cfg.enable {
     on = "push";
-    jobs.ci-cd.runs-on = "nixpkgs/nix-flakes";
+    jobs.ci-cd.runs-on = "ubuntu-latest";
     jobs.ci-cd.steps = [
       { uses = "actions/checkout@v1"; }
+      { 
+        uses = "cachix/install-nix-action@v15";
+        "with".nix_path = "channel:nixos-unstable";
+        "with".extra_nix_config = ''
+          access-tokens = github.com=${"$"}{{ secrets.GITHUB_TOKEN }}
+        '';
+      }
       { run = "nix develop"; }
       # this config comes from arguments
       { run = cfg.pre-build; }
