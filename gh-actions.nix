73907169b7878b89423d7499b679a87298f47b2c
hugosenari <hugosenari@gmail.com>
2021-11-18 13:52:38 -0300

    fix(gh-actions): enable flake

--- gh-actions.nix
+++ gh-actions.nix
@@ -5,10 +5,9 @@ in
   imports = [ ./gh-actions-options.nix ];
   config.files.yaml."/.github/workflows/ci-cd.yaml" = lib.mkIf cfg.enable {
     on = "push";
-    jobs.ci-cd.runs-on = "ubuntu-latest";
+    jobs.ci-cd.runs-on = "nixpkgs/nix-flakes";
     jobs.ci-cd.steps = [
       { uses = "actions/checkout@v1"; }
-      { uses = "cachix/install-nix-action@v13"; "with".nix_path = "channel:nixos-unstable"; }
       { run = "nix develop"; }
       # this config comes from arguments
       { run = cfg.pre-build; }
