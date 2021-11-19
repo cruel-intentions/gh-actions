a601a752e829484d24c77eccf2e8d78f48e409a9
hugosenari <hugosenari@gmail.com>
2021-11-18 21:17:03 -0300

    feat(gh-actions): multi files

--- gh-actions-options.nix
+++ gh-actions-options.nix
@@ -1,68 +1,77 @@
 { lib, ...}:
-{
-  options.gh-actions.ci-cd = lib.mkOption {
-    type = lib.types.submodule {
-      options.enable = lib.mkEnableOption "Github Actions CI-CD";
-      options.ssh-secret-name = lib.mkOption {
-        type = lib.types.nullOr lib.types.nonEmptyStr;
-        default = null;
-        example = "GH_ACTIONS_SSH_KEY";
-        description = ''
-          Name of GH Secret with PRIVATE SSH KEY
-          for more advanced usage try ssh option
-        '';
-      };
-      options.ssh = lib.mkOption {
-        type = lib.types.nullOr (lib.types.attrsOf lib.types.str);
-        default = null;
-        example.key = ''${"$"}{{ secret.GH_ACTIONS_SSH_KEY }}'';
-        description = ''
-          https://github.com/marketplace/actions/install-ssh-key
-          Config for ssh installation
-          There are two reasons to set it
-          1. our deploy runs in ssh
-          2. we have some private git repository
-
-          In this last case we should add your public key to some user with repository access (in github) or to our private server.
-        '';
-      };
-      options.on = lib.mkOption {
-        type = lib.types.attrsOf lib.types.anything;
-        default.push.branches = ["master"];
-        example.push.branches = ["master"];
-        description = "When this build should be triggered";
-      };
-      options.pre-build = lib.mkOption {
-        type = lib.types.nullOr lib.types.nonEmptyStr;
-        default = null;
-        example = "npm i";
-        description = "Command to run before build";
-      };
-      options.build = lib.mkOption {
-        type = lib.types.nullOr lib.types.nonEmptyStr;
-        default = null;
-        example = "npm run build";
-        description = "Command to run as build step";
-      };
-      options.test = lib.mkOption {
-        type = lib.types.nullOr lib.types.nonEmptyStr;
-        default = null;
-        example = "npm test";
-        description = "Command to run as test step";
-      };
-      options.deploy = lib.mkOption {
-        type = lib.types.nullOr lib.types.nonEmptyStr;
-        default = null;
-        example = "aws s3 sync ./build s3://my-bucket";
-        description = "Command to run as deploy step";
-      };
-      options.post-deploy = lib.mkOption {
-        type = lib.types.nullOr lib.types.nonEmptyStr;
-        default = null;
-        example = "echo Im done";
-        description = "Command that run after deploy";
-      };
+let
+  workflow = lib.types.submodule {
+    options.enable = lib.mkEnableOption "Github Actions CI-CD";
+    options.ssh-secret-name = lib.mkOption {
+      type = lib.types.nullOr lib.types.nonEmptyStr;
+      default = null;
+      example = "GH_ACTIONS_SSH_KEY";
+      description = ''
+        Name of GH Secret with PRIVATE SSH KEY
+        for more advanced usage try ssh option
+      '';
     };
+    options.ssh = lib.mkOption {
+      type = lib.types.nullOr (lib.types.attrsOf lib.types.str);
+      default = null;
+      example.key = ''${"$"}{{ secret.GH_ACTIONS_SSH_KEY }}'';
+      description = ''
+        https://github.com/marketplace/actions/install-ssh-key
+        Config for ssh installation
+        There are two reasons to set it
+        1. our deploy runs in ssh
+        2. we have some private git repository
+
+        In this last case we should add your public key to some user with repository access (in github) or to our private server.
+      '';
+    };
+    options.env = lib.mkOption {
+      type = lib.types.nullOr (lib.types.attrsOf lib.types.str);
+      default = null;
+      example.GIPHY_TOKEN = ''${"$"}{{ secret.GH_ACTIONS_SSH_KEY }}'';
+      description = ''Env variable used by steps'';
+    };
+    options.on = lib.mkOption {
+      type = lib.types.attrsOf lib.types.anything;
+      default.push.branches = ["master"];
+      example.push.branches = ["master"];
+      description = "When this build should be triggered";
+    };
+    options.pre-build = lib.mkOption {
+      type = lib.types.nullOr lib.types.nonEmptyStr;
+      default = null;
+      example = "npm i";
+      description = "Command to run before build";
+    };
+    options.build = lib.mkOption {
+      type = lib.types.nullOr lib.types.nonEmptyStr;
+      default = null;
+      example = "npm run build";
+      description = "Command to run as build step";
+    };
+    options.test = lib.mkOption {
+      type = lib.types.nullOr lib.types.nonEmptyStr;
+      default = null;
+      example = "npm test";
+      description = "Command to run as test step";
+    };
+    options.deploy = lib.mkOption {
+      type = lib.types.nullOr lib.types.nonEmptyStr;
+      default = null;
+      example = "aws s3 sync ./build s3://my-bucket";
+      description = "Command to run as deploy step";
+    };
+    options.post-deploy = lib.mkOption {
+      type = lib.types.nullOr lib.types.nonEmptyStr;
+      default = null;
+      example = "echo Im done";
+      description = "Command that run after deploy";
+    };
+  };
+in
+{
+  options.gh-actions = lib.mkOption {
+    type = lib.types.attrsOf workflow;
     default = {};
     description = "Configure your github actions CI/CD";
   };
--- gh-actions.nix
+++ gh-actions.nix
@@ -1,49 +1,73 @@
 { lib, config, ... }:
 let
-  cfg = config.gh-actions.ci-cd;
-  cmd = step: "nix develop --command gh-actions-ci-cd-${step}";
-  ifString = value: lib.mkIf (builtins.isString value) value;
+  workflows = config.gh-actions;
   arrOfIf = predicate: value: if predicate then [ value ] else [];
   arrOfIfAttr = value: arrOfIf (builtins.isAttrs value);
   arrOfIfStr = value: arrOfIf (builtins.isString value);
-  needs-ssh-key = arrOfIfAttr cfg.ssh {
-    name = "Install SSH Key";
-    uses = "shimataro/ssh-key-action@3c9b0fc6f2d223b8450b02a0445f526350fc73e0";
-    "with" = cfg.ssh;
+  yaml-file = name: cfg:
+  let
+    sshInfo = if(builtins.isString cfg.ssh-secret-name)
+    then {
+      key = ''${"$"}{{ secrets.${cfg.ssh-secret-name} }}'';
+      known_hosts = "unnecessary";
+    }
+    else cfg.ssh;
+    needs-ssh-key = arrOfIfAttr sshInfo {
+      name = "Install SSH Key";
+      uses = "shimataro/ssh-key-action@3c9b0fc6f2d223b8450b02a0445f526350fc73e0";
+      "with" = sshInfo;
+    };
+    env-vars = lib.mkIf (builtins.isAttrs cfg.env) cfg.env;
+    cmd = step: "nix develop --command gh-actions-${step}";
+    pre-build = arrOfIfStr cfg.pre-build
+      { run = cmd "${name}-pre-build"; name = "Pre Build"; env = env-vars; };
+    build = arrOfIfStr cfg.build
+      { run = cmd "${name}-build"; name = "Build"; env = env-vars; };
+    test = arrOfIfStr cfg.test
+      { run = cmd "${name}-test"; name = "Test"; env = env-vars; };
+    deploy = arrOfIfStr cfg.deploy
+      { run = cmd "${name}-deploy"; name = "Deploy"; env = env-vars; };
+    post-deploy = arrOfIfStr cfg.post-deploy
+      { run = cmd "${name}-post-deploy"; name = "Pos eploy"; env = env-vars; };
+    checkout = [ { uses = "actions/checkout@v2.4.0"; } ];
+    install-nix = [{ 
+      uses = "cachix/install-nix-action@v15";
+      "with".nix_path = "channel:nixos-unstable";
+      "with".extra_nix_config = ''
+        access-tokens = github.com=${"$"}{{ secrets.GITHUB_TOKEN }}
+      '';
+    }];
+  in
+  {
+    "/.github/workflows/${name}.yaml" = lib.mkIf cfg.enable {
+      on = cfg.on;
+      jobs.ci-cd.runs-on = "ubuntu-latest";
+      jobs.ci-cd.steps = needs-ssh-key
+        ++ checkout
+        ++ install-nix
+        ++ pre-build
+        ++ build
+        ++ test
+        ++ deploy
+        ++ post-deploy
+      ;
+    };
   };
+  yamls = lib.mapAttrsToList yaml-file workflows;
+  alias = name: cfg:
+  let ifString = predicate: value: lib.mkIf (predicate && builtins.isString value) value;
+  in
+  {
+    "gh-actions-${name}-pre-build"   = ifString cfg.enable cfg.pre-build;
+    "gh-actions-${name}-build"       = ifString cfg.enable cfg.build;
+    "gh-actions-${name}-test"        = ifString cfg.enable cfg.test;
+    "gh-actions-${name}-deploy"      = ifString cfg.enable cfg.deploy;
+    "gh-actions-${name}-post-deploy" = ifString cfg.enable cfg.post-deploy;
+  };
+  aliasses = lib.mapAttrsToList alias workflows;
 in
 { 
   imports = [ ./gh-actions-options.nix ];
-  config.gh-actions.ci-cd.ssh = lib.mkIf (builtins.isString cfg.ssh-secret-name) {
-    key = ''${"$"}{{ secrets.${cfg.ssh-secret-name} }}'';
-    known_hosts = "unnecessary";
-  };
-  config.files.alias = lib.mkIf cfg.enable {
-    gh-actions-ci-cd-pre-build = ifString cfg.pre-build;
-    gh-actions-ci-cd-build = ifString cfg.build;
-    gh-actions-ci-cd-test = ifString cfg.test;
-    gh-actions-ci-cd-deploy = ifString cfg.deploy;
-    gh-actions-ci-cd-post-deploy = ifString cfg.post-deploy;
-  };
-  config.files.yaml."/.github/workflows/ci-cd.yaml" = lib.mkIf cfg.enable {
-    on = cfg.on;
-    jobs.ci-cd.runs-on = "ubuntu-latest";
-    jobs.ci-cd.steps = needs-ssh-key ++ [
-      { uses = "actions/checkout@v2.4.0"; }
-      { 
-        uses = "cachix/install-nix-action@v15";
-        "with".nix_path = "channel:nixos-unstable";
-        "with".extra_nix_config = ''
-          access-tokens = github.com=${"$"}{{ secrets.GITHUB_TOKEN }}
-        '';
-      }
-    ]
-      # this config comes from arguments
-      ++ (arrOfIfStr cfg.pre-build { run = cmd "pre-build"; name = "Pre Build"; })
-      ++ (arrOfIfStr cfg.build { run = cmd "build"; name = "Build"; })
-      ++ (arrOfIfStr cfg.test { run = cmd "test"; name = "Test"; })
-      ++ (arrOfIfStr cfg.deploy { run = cmd "deploy"; name = "Deploy"; })
-      ++ (arrOfIfStr cfg.post-deploy { run = cmd "post-deploy"; name = "Pos eploy"; })
-    ;
-  };
+  config.files.alias = lib.foldAttrs lib.mergeAttrs {} aliasses;
+  config.files.yaml = lib.foldAttrs lib.mergeAttrs {} yamls;
 }
