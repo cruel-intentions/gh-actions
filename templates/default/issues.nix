200ecbb6efa6c2b478846bdf85b43a4a812d15a0
hugosenari <hugosenari@gmail.com>
2022-01-04 20:35:55 -0300

    fix(templates): add more question to template

--- templates/default/issues.nix
+++ templates/default/issues.nix
@@ -1,3 +1,15 @@
+let 
+  useless = [
+    "Would you willing to pay me a coffe for it?"
+    "What about pizzas?"
+    "Is your spiritual animal offended with previous question?"
+    "Are your spiritual animal already working on it in your imagination?"
+    "Are you wondering why I'm asking this questions?"
+    "Are you asking yourself if you should give up of opening it?"
+    "Isn't your eye bleeding yet?"
+    "Should I add more of these questions?"
+  ];
+in
 {
   gh-form.bug.description = "Report bugs";
   gh-form.bug.title = "bug: ";
@@ -10,10 +22,11 @@
   gh-form.bug.text.expected-behavior.required = true;
   gh-form.bug.text.reproduce.label = "How to reproduce";
   gh-form.bug.text.reproduce.description = "Describe how to reproduce";
-  gh-form.bug.checkboxes.more-info.label = "Please fill following questions";
+  gh-form.bug.checkboxes.more-info.label = "Please awnser following questions";
   gh-form.bug.checkboxes.more-info.options = [
-    "Is this bug critical for your workflow"
-  ];
+    "Is this bug critical for your workflow?"
+    "Would you willing to open a PR?"
+  ] ++ useless;
   gh-form.feat.description = "Feature Request";
   gh-form.feat.title = "feat: ";
   gh-form.feat.labels = ["feat"];
@@ -21,8 +34,8 @@
   gh-form.feat.text.description.label = "Describe your proposed solution";
   gh-form.feat.text.description.required = true;
   gh-form.feat.text.some-alt.label = "How you are overcomming this problem";
-  gh-form.feat.checkboxes.more-info.label = "Please fill following questions";
+  gh-form.feat.checkboxes.more-info.label = "Please anwser following questions";
   gh-form.feat.checkboxes.more-info.options = [
-    "Would you willing to pay"
-  ];
+    "Would you willing to open a PR?"
+  ] ++ useless;
 }
