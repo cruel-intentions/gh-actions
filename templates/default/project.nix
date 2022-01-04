7ec7f73acfa58bad9b269a9c9db9239c7644618f
hugosenari <hugosenari@gmail.com>
2022-01-04 16:38:40 -0300

    feat(templates): make default action corretly tag

--- templates/default/project.nix
+++ templates/default/project.nix
@@ -7,7 +7,25 @@
   gh-actions.tag-me.build = ''
     # tag this project on push to master
     # this is a bash script
-    git tag v$(convco version --bump)
+
+    CURR=`convco version`
+    NEXT=`convco version --bump`
+    MAJOR=`convco version --bump --major`
+    MINOR=`convco version --bump --minor`
+    PATCH=`convco version --bump --patch`
+
+    LOGS=`git log v$CURR..HEAD --format=oneline|cut -d' ' -f2`
+    if echo $CURR|grep -q $NEXT; then
+      echo "no reason to update tag" $CURR 
+      git log v$CURR..HEAD --format=oneline
+      exit 0
+    fi
+
+    NEXT=`echo $LOGS | grep -q "feat" && echo MINOR || echo $NEXT`
+    NEXT=`echo $LOGS | grep -q "!:"   && echo MAJOR || echo $NEXT`
+
+    git tag v$NEXT
+
     git push --tag
   '';
 }
