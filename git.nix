f8e24d0aaa5ffeea64ac5c78582c7275ae11e2ac
hugosenari <hugosenari@gmail.com>
2022-06-12 03:48:20 -0300

    chore: styling

--- git.nix
+++ git.nix
@@ -3,16 +3,17 @@
   files.gitignore.enable = true;
   # copy contents from https://github.com/github/gitignore
   # to our .gitignore
-  files.gitignore.pattern.".direnv" = true;
-  files.gitignore.pattern.".envrc"  = true;
+  files.gitignore.pattern.".direnv"          = true;
+  files.gitignore.pattern.".envrc"           = true;
   files.gitignore.template."Global/Archives" = true;
-  files.gitignore.template."Global/Backup" = true;
-  files.gitignore.template."Global/Diff" = true;
+  files.gitignore.template."Global/Backup"   = true;
+  files.gitignore.template."Global/Diff"     = true;
+
   # now we can use 'convco' command https://convco.github.io
   files.cmds.convco = true;
   # now we can use 'featw' command as alias to convco
-  files.alias.feat = ''convco commit --feat $@'';
-  files.alias.fix = ''convco commit --fix $@'';
   files.alias.chore = ''convco commit --chore $@'';
-  files.alias.docs = ''convco commit --docs $@'';
+  files.alias.docs  = ''convco commit --docs $@'';
+  files.alias.feat  = ''convco commit --feat $@'';
+  files.alias.fix   = ''convco commit --fix $@'';
 }
